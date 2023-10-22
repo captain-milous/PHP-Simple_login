a23ebdf92d2a4ef3603d0c143e459273052c6000
captain-milous <milda.tesar3@gmail.com>
2023-10-22 15:02:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,23 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Simple Login</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+
 </head>
 <body>
+    
     <div id="form">
         <h1>Login</h1>
         <form name="form" method="POST">
             <label>Username: </label>
             <input type="text" id="user" name="user">
+            <br><br>
             <label>Password: </label>
             <input type="password" id="pass" name="pass">
+            <br><br>
+            <input type="submit" id="btn" value="login" name="submit">
         </form>
     </div>
 
