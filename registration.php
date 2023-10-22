46a9e015877f70f1b418d84612d00e8aec237e99
captain-milous <milda.tesar3@gmail.com>
2023-10-22 16:14:23 +0200

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,33 @@
+<?php
+ include("connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Simple Registration</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+<body>
+
+    <div id="form">
+        <h1>Registrace</h1>
+        <form name="form-reg" action="login.php" method="POST">
+            <label>Email: </label>
+            <input type="email" id="user" name="user">
+            <br><br>
+            <label>Heslo: </label>
+            <input type="password" id="pass" name="pass">
+            <br><br>
+            <input type="submit" id="btn" value="Registrovat se" name="submit">
+        </form>
+        <br>
+        <small><a href="index.php">Zpět na přihlášení.</a></small>
+    </div>
+
+</body>
+</html>
