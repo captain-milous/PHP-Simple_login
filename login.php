1e0568875be33babb9653a8d14e88b13dae13181
captain-milous <milda.tesar3@gmail.com>
2023-10-22 15:58:48 +0200

    welcome

--- login.php
+++ login.php
@@ -3,6 +3,20 @@
     if(isset($_POST["submit"])) {
         $username = $_POST['user'];
         $password = $_POST['pass'];
+
+        $sql = "select * from user where email = '$username' and password = '$password'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+        if($count == 1) {
+            header("Location: welcome.php");
+        } else {
+            
+            echo "<script>
+                window.location.href = index.php;
+                alert('Špatné přihlašovací údaje.');
+            </script>";
+        }
     }
 
 
---
+++ welcome.php
@@ -0,0 +1,25 @@
+<?php
+ include("connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Simple Login</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+<body>
+
+    <div id="form">
+        <h1>Úspěšne přihlášen!</h1>
+        <a href="index.php" >  
+            <button id="btn">Zpět na přihlášení</button>
+        </a> 
+    </div>
+
+</body>
+</html>
