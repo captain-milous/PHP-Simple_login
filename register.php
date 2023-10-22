b43c6e3459bd7c694da0ac0f103ced7dc3b15af3
captain-milous <milda.tesar3@gmail.com>
2023-10-22 16:43:31 +0200

    registrace

--- register.php
+++ register.php
@@ -2,29 +2,13 @@
     include("connect.php");
     if(isset($_POST["submit"])) {
         if($_POST['pass'] == $_POST['pass2']) {
-
+            echo '<script> document.getElementById("msg").innerHTML = "Registrace zatím nefunguje";</script>';
+            header("Location: welcome.php");
         } else {
             echo '<script>
                 window.location.href = "registration.php";
                 alert("Hesla se neshodují.");
             </script>';
         }
-/*
-        $username = $_POST['user'];
-        $password = $_POST['pass'];
-
-        $sql = "select * from user where email = '$username' and password = '$password'";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $count = mysqli_num_rows($result);
-        if($count == 1) {
-            header("Location: welcome.php");
-        } else {
-            
-            echo '<script>
-                window.location.href = "index.php";
-                alert("Špatné přihlašovací údaje.");
-            </script>';
-        }*/
     }
 ?>
