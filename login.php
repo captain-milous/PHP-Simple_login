21516693b768cbca7aac298304a336d7603ec925
captain-milous <milda.tesar3@gmail.com>
2023-10-23 10:51:56 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
         $username = $_POST['user'];
         $password = $_POST['pass'];
 
-        $sql = "select * from user where email = '$username' and password = '$password'";
+        $sql = "select * from users where email = '$username' and password = '$password'";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
